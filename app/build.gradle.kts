78650f3419111188ec071c530b1eaa83e54189fe
AlanCadena <alanmmcwalker@gmail.com>
2025-10-29 22:08:19 -0400

    añade dependencias de room database

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,6 +37,9 @@ android {
 }
 
 dependencies {
+    val room_version = "2.8.3"
+
+    implementation("androidx.room:room-runtime:$room_version")
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
